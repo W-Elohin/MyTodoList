4a4315f7396cf6888704ec413f0d54d1457722f5
W-Elohin <ciin5831075@gmail.com>
2026-05-16 15:20:20 +0900

    feat: add my day focus view

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,11 +3,16 @@ import { TodoList } from './pages/TodoList';
 import { CalendarPage } from './pages/CalendarPage';
 import { Archive } from './pages/Archive';
 import { TimelinePage } from './pages/TimelinePage';
+import { MyDayPage } from './pages/MyDayPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: TodoList,
+  },
+  {
+    path: '/my-day',
+    Component: MyDayPage,
   },
   {
     path: '/calendar',
